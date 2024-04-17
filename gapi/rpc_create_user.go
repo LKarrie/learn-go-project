865ae62d3b676644683d77b2b29576b129605f64
LKarrie <42830637+LKarrie@users.noreply.github.com>
2024-04-17 16:58:57 +0800

    format code (#6)
    * add dbdocs
    
    * add grpc proto file
    
    * add grpc server and user api
    
    * add grpc gateway and swagger
    
    * add validator
    
    * format code
    
    * use migrate in main.go
    
    * sqlc nullstring
    
    * add zerolog
    
    * add async worker
    
    * add tx create user, asynq logger, mail
    
    * add verify email
    
    * update sqlc version 2
    
    * add grpc api mock test
    
    * switch db driver from lib/pq to pgx
    
    * format code

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -40,7 +40,8 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 			opts := []asynq.Option{
 				asynq.MaxRetry(10),
 				asynq.ProcessIn(10 * time.Second),
-				asynq.Queue(worker.QueueCritical),
+				asynq.Queue(
+					worker.QueueCritical),
 			}
 			return server.taskDistributor.DistributorTaskSendVerifyEmail(ctx, taskPayload, opts...)
 		},
